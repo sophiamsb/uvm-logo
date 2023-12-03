91704e515dd24796616efe53ab36ca7fee9e94fd
sophiamsb <sophia.msaoubi@gmail.com>
2023-12-03 11:35:40 +0100

    get v path

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const mousePosText = document.getElementById('mouse-pos');
 let mousePos = {x:undefined, y:undefined};
 
+const svgEl = document.getElementById("el-v");
+var svgPath = svgEl.getAttribute('d');
+const svgPathText = document.getElementById('v-path');
+
+
 window.addEventListener('mousemove', (event) => {
     mousePos = { x: event.clientX, y: event.clientY};
     mousePosText.textContent = `(${mousePos.x}, ${mousePos.y})`;
+    svgPathText.textContent = `(${svgPath})`;
 })
