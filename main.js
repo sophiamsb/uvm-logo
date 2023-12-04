f0cd7ec3851d3aa8b87dbd97459e929b4b837ec2
sophia <sophia@sedna.de>
2023-12-04 16:27:22 +0100

    testing with a 3rd blob

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ let mousePos = { x: undefined, y: undefined };
 var moveX = undefined;
 var moveY = undefined;
 var timeout;
+let direction = { x: undefined, y: undefined };
+var xDirection = undefined;
+var yDirection = undefined;
+var oldX = 0;
+var oldY = 0;
+window.addEventListener("mousemove", getMouseDirection, false);
 
 
 /*
@@ -15,6 +21,13 @@ viewport size
 */
 const viewportSizeText = document.getElementById("viewport-size");
 let viewportSize = { width: undefined, height: undefined };
+window.addEventListener(
+  "resize",
+  function (event) {
+    viewportSizeText.textContent = `(${window.innerWidth}, ${window.innerHeight})`;
+  },
+  true
+);
 
 /*
 snap svg plugin
@@ -25,10 +38,12 @@ speed + animation parameter
 var svg = document.getElementById("blobs");
 var s = Snap(svg);
 var blob1 = Snap.select("#blob1");
-var blob2 = Snap.select("#blob3");
+var blob2 = Snap.select("#blob2");
+var blob3 = Snap.select("#blob3");
 var blob1Points = blob1.node.getAttribute("d");
 var blob2Points = blob2.node.getAttribute("d");
-var speed = 1000;
+var blob3Points = blob3.node.getAttribute("d");
+var speed = 500;
 easing = mina.linear;
 
 /*
@@ -40,44 +55,45 @@ var toBlob1 = function () {
 var toBlob2 = function () {
   blob1.animate({ d: blob1Points }, speed, easing);
 };
+var toBlob3 = function () {
+  blob1.animate({ d: blob3Points }, speed, easing);
+};
 
 /*
 pause animation path on mousestop 
 */
 function mouseStopped(){                                
   mouseMoveText.textContent = "mouse stopped";
-  blob1.stop(); 
-  blob2.stop(); 
+}
+
+function getMouseDirection(e) {
+  //deal with the horizontal case
+  if (oldX < e.pageX) {
+      xDirection = "right";
+      toBlob1();
+  } else {
+      xDirection = "left";
+      toBlob2();
+  }
+
+  //deal with the vertical case
+  if (oldY < e.pageY) {
+      yDirection = "down";
+      toBlob3();
+  } else {
+      yDirection = "up";
+  }
+
+  oldX = e.pageX;
+  oldY = e.pageY;
+  let direction = {x:xDirection, y:yDirection};
+
+  mouseDirText.TextContent = `${direction.x},${direction.y}`;
+  console.log(`${direction.x},${direction.y}`);
+  clearTimeout(timeout);
+  mouseMoveText.textContent = "mouse moving";
+  timeout = setTimeout(mouseStopped, 300);
 }
 
 
-/*
-animation path on mousemove 
-*/
-window.addEventListener("mousemove", (event) => {
-
-  moveX = event.clientX;
-  moveY = event.clientY;
-  mousePos = { x: moveX, y: moveY };
-  mousePosText.textContent = `(${mousePos.x}, ${mousePos.y})`;
-  clearTimeout(timeout);
-
-  if (moveX >= window.innerWidth / 2) {
-    toBlob1();
-    mouseDirText.textContent = "right";
-  } else if (moveX <= window.innerWidth / 2) {
-    toBlob2();
-    mouseDirText.textContent = "left";
-  }
-  mouseMoveText.textContent = "mouse moving";
-  timeout = setTimeout(mouseStopped, 300);
-});
-
-window.addEventListener(
-  "resize",
-  function (event) {
-    viewportSizeText.textContent = `(${window.innerWidth}, ${window.innerHeight})`;
-  },
-  true
-);
 
