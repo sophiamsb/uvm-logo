9f541260655df3716fb643efd556076d5ce5c2d4
sophiamsb <sophia.msaoubi@gmail.com>
2023-12-04 21:53:57 +0100

    test 4 blobs in each direction

--- main.js
+++ main.js
@@ -1,33 +1,11 @@
 /*
 mouse position & direction
 */
-const mousePosText = document.getElementById("mouse-pos");
-const mouseDirText = document.getElementById("mouse-dir");
-const mouseMoveText = document.getElementById("mouse-move");
-let mousePos = { x: undefined, y: undefined };
-var moveX = undefined;
-var moveY = undefined;
-var timeout;
-let direction = { x: undefined, y: undefined };
 var xDirection = undefined;
 var yDirection = undefined;
 var oldX = 0;
 var oldY = 0;
 window.addEventListener("mousemove", getMouseDirection, false);
-
-
-/*
-viewport size
-*/
-const viewportSizeText = document.getElementById("viewport-size");
-let viewportSize = { width: undefined, height: undefined };
-window.addEventListener(
-  "resize",
-  function (event) {
-    viewportSizeText.textContent = `(${window.innerWidth}, ${window.innerHeight})`;
-  },
-  true
-);
 
 /*
 snap svg plugin
@@ -40,9 +18,11 @@ var s = Snap(svg);
 var blob1 = Snap.select("#blob1");
 var blob2 = Snap.select("#blob2");
 var blob3 = Snap.select("#blob3");
+var blob4 = Snap.select("#blob4");
 var blob1Points = blob1.node.getAttribute("d");
 var blob2Points = blob2.node.getAttribute("d");
 var blob3Points = blob3.node.getAttribute("d");
+var blob4Points = blob4.node.getAttribute("d");
 var speed = 500;
 easing = mina.linear;
 
@@ -58,13 +38,11 @@ var toBlob2 = function () {
 var toBlob3 = function () {
   blob1.animate({ d: blob3Points }, speed, easing);
 };
+var toBlob4 = function () {
+  blob1.animate({ d: blob4Points }, speed, easing);
+};
 
-/*
-pause animation path on mousestop 
-*/
-function mouseStopped(){                                
-  mouseMoveText.textContent = "mouse stopped";
-}
+
 
 function getMouseDirection(e) {
   //deal with the horizontal case
@@ -80,19 +58,15 @@ function getMouseDirection(e) {
   if (oldY < e.pageY) {
       yDirection = "down";
       toBlob3();
-  } else {
+  } else if (oldY > e.pageY) {
       yDirection = "up";
+      toBlob4();
   }
 
   oldX = e.pageX;
   oldY = e.pageY;
-  let direction = {x:xDirection, y:yDirection};
 
-  mouseDirText.TextContent = `${direction.x},${direction.y}`;
-  console.log(`${direction.x},${direction.y}`);
-  clearTimeout(timeout);
-  mouseMoveText.textContent = "mouse moving";
-  timeout = setTimeout(mouseStopped, 300);
+
 }
 
 
