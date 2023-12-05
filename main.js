f8609dd1dda953408a7624843680e1732fd66253
sophia <sophia@sedna.de>
2023-12-05 15:08:12 +0100

    return to linear easing, on mouse over goes back to initial v shape

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 /*
 mouse position & direction
 */
-const lastPoint = {x: null, y: null}
+const lastPoint = { x: null, y: null };
+var isOnSvg = false;
 const mouseDirText = document.getElementById("mouse-dir");
 window.addEventListener("mousemove", getMouseDirection, false);
-
+document.getElementById("v").addEventListener("mouseover", function () {
+  isOnSvg = true;
+  console.log("on");
+});
+document.getElementById("v").addEventListener("mouseout", function () {
+  isOnSvg = false;
+  console.log("out");
+});
 
 /*
 snap svg plugin
@@ -25,57 +33,67 @@ var blob3Points = blob3.node.getAttribute("d");
 var blob4Points = blob4.node.getAttribute("d");
 var blob5Points = blob5.node.getAttribute("d");
 // var speed = 250;
-var speed= 1500;
+var speed = 1000;
 easingBounce = mina.bounce;
-easingElastic = mina.elastic;
+easing = mina.backout;
 
 /*
 animation path
 */
+var toBlobInitial = function () {
+  blob1.animate({ d: blob1Points }, speed, easing);
+};
 var toBlob1 = function () {
-  blob1.animate({ d: blob2Points }, speed, easingElastic);
+  blob1.animate({ d: blob2Points }, speed, easing);
 };
 var toBlob2 = function () {
-  blob1.animate({ d: blob5Points }, speed, easingElastic);
+  blob1.animate({ d: blob5Points }, speed, easing);
 };
 var toBlob3 = function () {
-  blob1.animate({ d: blob3Points }, speed, easingElastic);
+  blob1.animate({ d: blob3Points }, speed, easing);
 };
 var toBlob4 = function () {
-  blob1.animate({ d: blob4Points }, speed, easingElastic);
+  blob1.animate({ d: blob4Points }, speed, easing);
 };
 
-
 // a function to recalculate the canvas offsets
-function reOffset(){
+function reOffset() {
   svg.getBoundingClientRect();
-  offsetX=svg.left;
-  offsetY=svg.top;        
+  offsetX = svg.left;
+  offsetY = svg.top;
 }
 
 // listen for window resizing (and scrolling) events
 //     and then recalculate the canvas offsets
-window.onscroll=function(e){ reOffset(); }
-window.onresize=function(e){ reOffset(); }
+window.onscroll = function (e) {
+  reOffset();
+};
+window.onresize = function (e) {
+  reOffset();
+};
 
 function getMouseDirection(e) {
-
-  //deal with the horizontal case
-  if (e.clientX > lastPoint.x) {
+  if (isOnSvg === false) {
+    //deal with the horizontal case
+    if (e.clientX > lastPoint.x) {
       toBlob1();
       mouseDirText.textContent = "right";
-  } else if (e.clientX < lastPoint.x ) {
+    } else if (e.clientX < lastPoint.x) {
       toBlob2();
       mouseDirText.textContent = "left";
-  }
+    }
 
-  //deal with the vertical case
-  if ( e.clientY > lastPoint.y){
+    //deal with the vertical case
+    if (e.clientY > lastPoint.y) {
       toBlob3();
       mouseDirText.textContent = "down";
-  } else  if ( e.clientY < lastPoint.y ){
+    } else if (e.clientY < lastPoint.y) {
       toBlob4();
       mouseDirText.textContent = "up";
+    }
+  } else {
+    toBlobInitial();
+    mouseDirText.textContent = "mouse over";
   }
 
   /* 
@@ -83,13 +101,6 @@ function getMouseDirection(e) {
     then update the cursor position tracker for the 
     next iteration
   */
-
-  lastPoint.x = e.clientX
-  lastPoint.y = e.clientY
-
- 
-
+  lastPoint.x = e.clientX;
+  lastPoint.y = e.clientY;
 }
-
-
-
